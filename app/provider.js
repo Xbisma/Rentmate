ef4ee6790ab0571c0c48d0217c4150b084bb22c8
bisma maheen <bisma.slhry@gmail.com>
2025-12-20 09:04:47 +0500

    Clean up: Remove old root files, keep organized structure

--- app/provider.js
+++
@@ -1,8 +0,0 @@
-// app/providers.js
-"use client"; // This makes it a client component
-
-import { PropertyProvider } from './context/PropertyContext';
-
-export function Providers({ children }) {
-  return <PropertyProvider>{children}</PropertyProvider>;
-}
