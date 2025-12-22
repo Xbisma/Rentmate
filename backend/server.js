90fc5ebe4aaef09d8a9f0e239dd9e3a334a802b6
bisma maheen <bisma.slhry@gmail.com>
2025-12-22 19:17:05 +0500

    FINAL UPDATED server.js file with the completion of the backend module

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes.js";
-import rentalApplicationRoutes from "./routes/rentalApplicationRoutes.js";
-import dashboardRoutes from "./routes/dashboardRoutes.js";
 import propertyRoutes from "./routes/propertyRoutes.js";
-import notificationRoutes from "./routes/notificationRoutes.js";
+import tenantRoutes from "./routes/tenantRoutes.js";
+import rentalApplicationRoutes from "./routes/rentalApplicationRoutes.js";
 import maintenanceRoutes from "./routes/maintenanceRoutes.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
 import paymentRoutes from "./routes/paymentRoutes.js";
+import alertRoutes from "./routes/alertRoutes.js";
+import { startCronJobs } from "./utils/cronJobs.js";
+import dashboardRoutes from "./routes/dashboardRoutes.js";
 
 dotenv.config();
 
@@ -17,13 +21,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoutes);
-app.use("/api/applications", rentalApplicationRoutes);
-app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/properties", propertyRoutes);
-app.use("/api/notifications", notificationRoutes);
+app.use("/api/tenant", tenantRoutes);
+app.use("/api/applications", rentalApplicationRoutes);
 app.use("/api/maintenance", maintenanceRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/alerts", alertRoutes);
 app.use("/api/payments", paymentRoutes);
-
+app.use("/api/dashboard", dashboardRoutes);
 
 // connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
@@ -38,3 +43,6 @@ app.get("/", (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
+
+// start cron jobs
+startCronJobs();
